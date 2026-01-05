035d7ebc132ba90e3ffc3125dcd83dbcba999586
aryanraut01 <aryanraut435@gmail.com>
2026-01-05 13:41:34 +0530

    Add files via upload

---
+++ BIGO1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    int sum = a + b;   // constant operation
+    cout << "Sum = " << sum;
+
+    return 0;
+}
