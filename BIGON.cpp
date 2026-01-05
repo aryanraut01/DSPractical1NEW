545274e9930b66f8eeb94ebfe501a1d8fc53b9eb
aryanraut01 <aryanraut435@gmail.com>
2026-01-05 13:47:22 +0530

    Update AryanBIGON.cpp

--- BIGON.cpp
+++ BIGON.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int n = 5;
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = {11, 22, 33, 44, 55};
 
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
